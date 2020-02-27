8a96b452587ebbcc1852c8186d31f252ada8e22c
Michael Kennel <mikennel@Michaels-MacBook-Pro.local>
2020-02-26 20:07:14 -0500

    initial commit

---
+++ dist/precache-manifest.7e4b9e01c3b43825253d5779c0365451.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2987a1c05b481b6e2875",
+    "url": "/js/chunk-vendors.c9e92046.js"
+  },
+  {
+    "revision": "fa0b3651c498f5707025",
+    "url": "/js/chunk-e9bdc436.b3f559ea.js"
+  },
+  {
+    "revision": "d6798e8b29bad42d8e68",
+    "url": "/js/chunk-0b70a97a.4ab2c50e.js"
+  },
+  {
+    "revision": "986e1bfb5aa103568c0a",
+    "url": "/js/app.76787be6.js"
+  },
+  {
+    "revision": "498bda94f6679eebdaa6e1795b73cffd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2bff61d5d6c5f852622afd01501cd61",
+    "url": "/img/loading-dots.f2bff61d.svg"
+  },
+  {
+    "revision": "fa0b3651c498f5707025",
+    "url": "/css/chunk-e9bdc436.2d9d7354.css"
+  },
+  {
+    "revision": "d6798e8b29bad42d8e68",
+    "url": "/css/chunk-0b70a97a.640389d1.css"
+  },
+  {
+    "revision": "986e1bfb5aa103568c0a",
+    "url": "/css/app.c5f166e4.css"
+  }
+];
---
+++ src/components/routed/Public/Main.vue
@@ -0,0 +1,39 @@
+<script>
+import ProNav from '$common/ProNav'
+export default {
+  name: 'PublicMain',
+  components: {
+    'pro-nav': ProNav,
+  },
+  watch: {
+    '$route' (newRoute, oldRoute) {
+      const isHigherRoute = newRoute.meta.transIndex > oldRoute.meta.transIndex
+      this.transtionName = isHigherRoute ? 'slide-left' : 'slide-right'
+    },
+  },
+  data () {
+    return {
+      transtionName: 'slide-left',
+    }
+  },
+}
+</script>
+
+
+<template lang="pug">
+  .public-main-main
+    .public-main-container
+      router-view.router-view(
+        :key="$route.name"
+      )
+</template>
+
+<style lang="sass" scoped>
+  @import '$vars'
+  @import '$styles/transitions.sass'
+  .public-main-main
+    .public-main-container
+      background-color: white
+      .router-view
+        height: 100vh
+</style>
